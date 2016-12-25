5aaababb474c2a60f6e6fe3ee1050bcc06b390bd
Rohit Saraogi <rohitsaraogi101@gmail.com>
2016-12-25 15:22:09 +0530

    Create Assignment_2_3.java

---
+++ Assignment_2_3.java
@@ -0,0 +1,29 @@
+public class Assignment_2_3 {
+
+  public static void main(String[] args) {
+  int i,j,k=97,l=98,m=99;
+    for (int i = 1; i < 5; i++) {
+      for (int j = 1; j < 5; j++){
+      if((i == 1 && j == 3) || (i == 2 && j == 2)
+      || (i == 2 && j == 4)|| (i == 3 && j == 1)
+      || (i == 3 && j == 5)|| (i == 4 && j == 2)
+      || (i == 4 && j == 4)|| (i == 5 && j == 3)
+        
+	System.out.print((char)k);
+
+	else if ((i == 2 && j == 3) || (i == 3 && j == 2)
+      || (i == 3 && j == 4)|| (i == 4 && j == 3)
+
+      System.out.print((char)l);
+
+	else if (i ==3 && j==3)
+	System.out.print((char)m);
+
+	else
+	System.out.print((" ");
+      }
+      System.out.println();
+
+    }
+  }
+}
