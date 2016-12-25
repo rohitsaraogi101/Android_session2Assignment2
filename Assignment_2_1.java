71a48778ab2d6dcc2631650ba7cab709c83b5a48
Rohit Saraogi <rohitsaraogi101@gmail.com>
2016-12-25 14:25:47 +0530

    Create Assignment_2_1.java

---
+++ Assignment_2_1.java
@@ -0,0 +1,10 @@
+public class Assignment_2_1 {
+	public static void main(String args[])    /* main method */
+	{
+		if(System.out.printf("Rohit Saraogi") != null)     /* my name is Rohit Saraogi*/
+		{
+			
+		}
+	}
+
+}
